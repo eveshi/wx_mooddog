a94613a7b01f46b23842e692cff9e48173bb5325
Eve Shi <qianyiwork@gmail.com>
2018-02-03 22:08:41 +0800

    update

--- pages/history/hisDe/hisDe.js
+++ pages/history/hisDe/hisDe.js
@@ -1,25 +1,71 @@
 // pages/history/hisDe/hisDe.js
+var app = getApp()
+const imageLink = [
+  "../../../images/verySad.png",
+  "../../../images/sad.png",
+  "../../../images/calm.png",
+  "../../../images/happy.png",
+  "../../../images/veryHappy.png"
+]
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    id: 0,
+    contData: {},
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.setData({
+      id: options.id
+    })
+
+    let query = new wx.BaaS.Query()
+
+    query.compare("_id","=", this.data.id)
+
+    let tableID = 21194
+
+    var Product = new wx.BaaS.TableObject(tableID)
+    Product.setQuery(query).find().then((res) => {
+      var userData = res.data['objects'][0]
+      var newRow = {}
+      var date = new Date(userData.created_at*1000)
+      newRow.year = date.getFullYear()
+      newRow.month = date.getMonth()+"月"
+      newRow.day = date.getDate()+"日"
+      newRow.hour = date.getHours()
+      newRow.min = date.getMinutes()
+      newRow.sec = date.getSeconds()
+      newRow.mood = imageLink[userData.moodSave]
+      newRow.content = userData.content
+      newRow.tgts1 = userData.tgts1
+      newRow.tgts1Change = userData.tgts1Change
+      newRow.tgts1TrapId = userData.tgts1TrapId
+      newRow.tgts2 = userData.tgts2
+      newRow.tgts2Change = userData.tgts2Change
+      newRow.tgts2TrapId = userData.tgts2TrapId
+      newRow.tgts3 = userData.tgts3
+      newRow.tgts3Change = userData.tgts3Change
+      newRow.tgts3TrapId = userData.tgts3TrapId
+      this.setData({
+        contData: newRow
+      })
+      console.log(this.data.contData)
+    }, (err) => {
+      // err
+    })
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
--- pages/history/history.js
+++ pages/history/history.js
@@ -16,19 +16,95 @@ Page({
   data: {
     filter: [
       { name: "all", value: "全部"},
-      { name: "4", value: "好开心"},
-      { name: "3", value: "开心"},
-      { name: "2", value: "一般"},
-      { name: "1", value: "不开心" },
-      { name: "0", value: "好不开心"},
+      { name: 4, value: "好开心"},
+      { name: 3, value: "开心"},
+      { name: 2, value: "一般"},
+      { name: 1, value: "不开心" },
+      { name: 0, value: "好不开心"},
     ],
     index: 0,
-    userHistory:[],
+    userHistoryModel: [],
+    userHistory: [],
+    id: 0,
   },
 
   moodChange: function(e){
     this.setData({
       index: e.detail.value
+    })
+    var histo = this.data.userHistoryModel
+    var histo2 = []
+    //需要重新过滤月份和年份
+    for(var i=0; i<histo.length; i++){
+      if(histo[i].show != "true"){
+        if(this.data.filter[this.data.index].name == "all"){
+          histo[i].moodShow = "true"
+          histo2.push(histo[i])
+        }
+        else if(histo[i].mood != this.data.filter[this.data.index].name){
+          histo[i].moodShow = "false"
+        }
+        else if(histo[i].mood == this.data.filter[this.data.index].name){
+          histo[i].moodShow = "true"
+          histo2.push(histo[i])
+        }
+      }
+    }
+    console.log(histo2)
+    var newArray = []
+    if(histo2.length == 0){
+      newArray.push({content:"无记录", moodShow:"true"})
+    }
+    else if(histo2 == histo){
+      newArray = histo
+    }
+    else{
+      //重排月份
+      var yH = histo2[histo2.length - 1].year
+      var yL = histo2[0].year
+      newArray = []
+      var newCut
+      var flag
+      for (; yH > yL - 1; yH--) {
+        console.log(yH)
+        for (var a = 12; a > 0; a--) {
+          flag = false
+          for (var i = histo2.length - 1; i > -1; i--) {
+            if (histo2[i].month == a) {
+              console.log(histo2[i].month)
+              newCut = { year:0, month:0, day: "", content: "", mood: 0, moodImg: "", _id: "", moodShow: "true" }
+              if (flag == false) {
+                newArray.push({ year: yH, month: a + "月  ", show: "true" })
+                flag = true
+              }
+              newCut.year = histo2[i].year
+              newCut.month = histo2[i].month
+              newCut.day = histo2[i].day
+              newCut.content = histo2[i].content
+              newCut.mood = histo2[i].mood
+              newCut.moodImg = histo2[i].moodImg
+              newCut._id = histo2[i]._id
+              newArray.push(newCut)
+            }
+          }
+        }
+      }
+    }
+    this.setData({
+      userHistory: newArray
+    })
+  },
+
+  details: function(e){
+    console.log(e.target.dataset._id)
+    this.setData({
+      id: e.target.dataset._id
+    })
+    console.log(this.data.id)
+    var itemId = this.data.id
+    // console.log(itemId._id)
+    wx.navigateTo({
+      url: 'hisDe/hisDe?id='+itemId,
     })
   },
 
@@ -38,10 +114,6 @@ Page({
   onLoad: function (options) {
 
     let userInfo = wx.BaaS.storage.get('userinfo')
-    // wx.showLoading({
-    //   title: '二狗努力中~',
-    //   mask: true
-    // })
     // 尝试获取用户信息，若获取失败，则使用 wx.BaaS.login 进行登录
     if (userInfo) {
       let {
@@ -50,7 +122,6 @@ Page({
       this.setData({
         id
       })
-    console.log(id)
     }
     else {
       wx.BaaS.login().then(res => {
@@ -60,7 +131,6 @@ Page({
         this.setData({
           id
         })
-        console.log(id)
       }).catch(err => console.log(err))
     }
 
@@ -68,7 +138,6 @@ Page({
     let query = new wx.BaaS.Query()
 
     var id = parseInt(userInfo.id)
-    console.log(id)
 
     query.compare("created_by","=",id)
 
@@ -78,12 +147,9 @@ Page({
     Product.setQuery(query).find().then((res) => {
       var userData = res.data
       var objectLen = userData['objects'].length
-      var historySingle = { year: 0, month:0, day:0, content: "", mood: "" }
+      var historySingle = { year: 0, month:0, day:0, content: "", mood: 0, moodImg: "", _id:"" }
       var history = []
-      var mood = 0
       var date
-      console.log(userData)
-      console.log(objectLen)
       // 创建新数组
       for(var i=0; i<objectLen; i++){
         date = new Date(userData['objects'][i].created_at*1000)
@@ -91,38 +157,46 @@ Page({
         historySingle.month = date.getMonth()+1
         historySingle.day = date.getDate()
         historySingle.content = userData['objects'][i].content
-        mood = userData['objects'][i].moodSave
-        historySingle.mood = imageLink[mood]
+        historySingle.mood = userData['objects'][i].moodSave
+        historySingle.moodImg = imageLink[historySingle.mood]
+        historySingle._id = userData['objects'][i]._id
         history.push(historySingle)
-        historySingle = { year: "", month: "", day: "", content: "", mood: "" }
+        historySingle = { year: 0, month: 0, day: 0, content: "", mood: 0, moodImg: "", _id: "" }
       }
+      console.log(history)
       // 筛选出年份和月份
-      var yL = history[history.length-1].year
-      var yH = history[0].year
+      var yH = history[history.length-1].year
+      var yL = history[0].year
       var newArray = []
       var newCut
       var flag
       for(;yH>yL-1;yH--){
-        flag = false 
-        for(var i=0; i<history.length; i++){
-          if(history[i].year == yL){
-            for(var a=1; a<13; a++){
-              if(history[i].month == a){
-                newCut = { day: "", content: "", mood: "" }
-                if(flag == false){
-                  newArray.push({ year: yL, month: a })
-                  flag = true
-                }
-                newCut.day = history[i].day
-                newCut.content = history[i].content
-                newCut.mood = history[i].mood
-                newArray.push(newCut)
+        console.log(yH)
+        for(var a=12;a>0;a--){
+          flag = false
+          for(var i=history.length-1; i>-1; i--){
+            if(history[i].month == a){
+              console.log(history[i].month)
+              newCut = { year:0, month: 0, day: "", content: "", mood: 0, moodImg: "", _id: "", moodShow: "true" }
+              if (flag == false) {
+                newArray.push({ year: yH, month: a + "月  ", show: "true" })
+                flag = true
               }
+              newCut.year = history[i].year
+              newCut.month = history[i].month
+              newCut.day = history[i].day + "号"
+              newCut.content = history[i].content
+              newCut.mood = history[i].mood
+              newCut.moodImg = history[i].moodImg
+              newCut._id = history[i]._id
+              newArray.push(newCut)
             }
           }
         }
       }
+      console.log(newArray)
       this.setData({
+        userHistoryModel: newArray,
         userHistory: newArray
       })
     }, (err) => {
